5abc99a9aac9bcf04e65c44d71a5fed72b3e10a9
DNews005 <dnews005@fiu.edu>
2024-09-23 14:33:21 -0400

    Delete geek-text directory

--- geek-text/src/main/java/group9/geek_text/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package group9.geektext.repository;
-
-import group9.geektext.entity.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
--- geek-text/src/main/java/group9/geek_text/repository/ShoppingCartRepository.java
+++
@@ -1,8 +0,0 @@
-package group9.geektext.repository;
-
-import group9.geektext.entity.ShoppingCart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
-    ShoppingCart findByUser_Id(Long userId);  // Find shopping cart by user ID
-}
