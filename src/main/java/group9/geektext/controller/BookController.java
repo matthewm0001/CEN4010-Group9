0e3611b2baa2d0073ad08bda54c98e7e287c9b14
Matthew Miller <mmill259@fiu.edu>
2024-11-15 00:52:30 -0500

    Fix some issues with output for books

--- src/main/java/group9/geektext/controller/BookController.java
+++ src/main/java/group9/geektext/controller/BookController.java
@@ -1,6 +1,7 @@
 package group9.geektext.controller;
 
 import group9.geektext.dto.BookDTO;
+import group9.geektext.dto.BookDTOStandalone;
 import group9.geektext.service.BookService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,14 +20,14 @@ public class BookController {
 
     // Get all books
     @GetMapping
-    public List<BookDTO> getAllBooks() {
+    public List<BookDTOStandalone> getAllBooks() {
         return bookService.getAllBooks();
     }
 
     // Get a single book by ID
     @GetMapping("/{id}")
-    public ResponseEntity<BookDTO> getBookById(@PathVariable Long id) {
-        BookDTO book = bookService.getBookById(id);
+    public ResponseEntity<BookDTOStandalone> getBookById(@PathVariable Long id) {
+        BookDTOStandalone book = bookService.getBookById(id);
         if (book != null) {
             return ResponseEntity.ok(book);
         } else {
@@ -35,16 +36,16 @@ public class BookController {
     }
 
     @PostMapping
-    public ResponseEntity<BookDTO> createBook(@RequestBody BookDTO book) {
+    public ResponseEntity<BookDTOStandalone> createBook(@RequestBody BookDTO book) {
         // Create a new book
-        BookDTO createdBook = bookService.createBook(book);
+        BookDTOStandalone createdBook = bookService.createBook(book);
         return ResponseEntity.ok(createdBook);
     }
 
     // Update a book
     @PutMapping("/{id}")
-    public ResponseEntity<BookDTO> updateAuthor(@PathVariable Long id, @RequestBody BookDTO bookDTO) {
-        BookDTO updatedBook = bookService.updateBook(id, bookDTO);
+    public ResponseEntity<BookDTOStandalone> updateAuthor(@PathVariable Long id, @RequestBody BookDTO bookDTO) {
+        BookDTOStandalone updatedBook = bookService.updateBook(id, bookDTO);
         return updatedBook != null ? ResponseEntity.ok(updatedBook) : ResponseEntity.notFound().build();
     }
 
--- src/main/java/group9/geektext/dto/AuthorDTO.java
+++ src/main/java/group9/geektext/dto/AuthorDTO.java
@@ -1,19 +1,82 @@
 package group9.geektext.dto;
+import java.util.List;
 
 public class AuthorDTO {
-    private String fullName; // Combined firstName and lastName
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String biography;
+    private String publisher;
+    private List<BookDTO> books;  // List of books the author has written
 
     // Constructor
-    public AuthorDTO(String firstName, String lastName) {
-        this.fullName = firstName + " " + lastName; // Concatenate first and last name
+    public AuthorDTO() {
     }
 
-    // Getter for full name
-    public String getFullName() {
-        return fullName;
+    // Constructor
+    public AuthorDTO(Long id, String firstName, String lastName, String biography, String publisher) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.biography = biography;
+        this.publisher = publisher;
     }
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
+    public AuthorDTO(Long id, String firstName, String lastName, String biography, String publisher, List<BookDTO> books) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.biography = biography;
+        this.publisher = publisher;
+        this.books = books;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public List<BookDTO> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<BookDTO> books) {
+        this.books = books;
     }
 }
